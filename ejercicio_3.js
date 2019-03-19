a77e2c6d5f8555f8beaed1f8ca1fc42b1cdd1283
jpgarcia89 <jpgarcia89@hotmail.com>
2019-03-19 10:58:05 -0300

    ej3 resuelto eq 1

--- ejercicio_3.js
+++ ejercicio_3.js
@@ -27,3 +27,91 @@ Utilice estos ejemplos como resultados válidos para testear su aplicación.
 ** IMPORTANTE: Antes de escribir su código, cree un branch con su nombre o el de su equipo. Luego deberá subir al repositorio su resolución!
 
 */
+
+
+console.log(tiempoEspera([5,3,4], 1)) //--> deberá devolver 12 ya que como sólo hay una caja se suman la cantidad de minutos consumidos por cliente.
+console.log(tiempoEspera([10,2,3,3], 2)) //--> deberá devolver 10 ya que hay 2 cajas. Los clientes 2do, 3ero y 4to finalizan antes que el 1ero.
+console.log(tiempoEspera([2,3,10], 2)) //--> deberá devolver 12
+
+
+function tiempoEspera(cli, ncajas) 
+{
+    var tiempo = 0;
+    var cajas = [];
+
+    for (let i = 0; i < ncajas; i++) {
+        cajas.push(0);        
+    }
+
+    cli=cli.reverse();
+    while (cli.length>0  || cajas.some(r=>r>0)) {
+        
+        // cajas.forEach(caja => {
+        //     if (caja===0) {
+        //         caja = cli.pop()
+        //     }
+        //     else{
+        //         caja--;
+        //     }
+        // });
+
+        for (let i = 0; i < cajas.length; i++) {
+            if (cajas[i]===0) {
+                cajas[i] = cli.pop()
+                cajas[i]--;
+            }
+            else{
+                cajas[i]--;
+            }
+            
+        }
+
+        tiempo++;
+        
+
+    }
+
+
+    return tiempo;
+
+
+
+    // var res = 0;
+    // var cajaActiva = 1;
+    // if(ncajas < 2){
+    //     arr_tiempos.forEach(item => {
+    //         res+=item;
+    //     });
+       
+    // }
+    // else{
+    //     for(i=0;i<arr_tiempos.length;i++){
+    //         if(arr_tiempos[i] <= arr_tiempos[i+1]){
+    //             cajaActiva +=1
+    //             res+=arr_tiempos[i];
+               
+    //         }else{
+    //             console.log(arr_tiempos[i])
+               
+    //         }
+    //     }
+       
+    // }
+    // return res;
+    /*
+    var time= 0;
+
+    var caja;
+
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[index];
+
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
+        
+    }
+    */
+}
