d1a6fafae346e2e93a5f251075c68cfcfbf08ae6
juanmanuelariza <juanma.ariza23@gmail.com>
2019-03-19 10:36:54 -0300

    Equipo 6 Ejercicio 2

--- ejercicio_2.js
+++ ejercicio_2.js
@@ -21,3 +21,28 @@ Ejemplo:
 ** IMPORTANTE: Antes de escribir su código, cree un branch con su nombre o el de su equipo. Luego deberá subir al repositorio su resolución!
 
 */
+
+//letraabc(['a', 'b', 'c', 'd', 'f']);
+letraabc(['o','p','r','s']);
+
+function letraabc(Arrayabc) {
+    // var bonus = true;
+    var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var posicionLetra;
+    var x = true;
+    var result;
+    posicionLetra = abc.indexOf(Arrayabc[0]);
+    for (let i = 1; i < Arrayabc.length; i++) {
+        if(x){
+            if (posicionLetra == abc.indexOf(Arrayabc[i]) - 1) {
+                posicionLetra = abc.indexOf(Arrayabc[i]) - 1;
+            }
+            else{
+                x = false;
+                result = abc[abc.indexOf(Arrayabc[i]) - 1];
+            }
+        }
+    }
+    console.log(result);
+}
+
